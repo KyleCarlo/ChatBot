a2ba445f659306080e37ac833d9508576b9b0ad7
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-03-31 10:38:24 +0800

    first commit

---
+++ MCO2.pl
@@ -0,0 +1,43 @@
+count_predicates(Count) :-
+    findall(_, tb(_), Predicates),
+    length(Predicates, Count).
+
+demographics(Name, Age, Sex) :-
+    write('What is your name? '),
+    read(Name),
+    write('What is your age? '),
+    read(Age),
+    write('What is your sex? (M/F) '),
+    read(Sex).
+
+askSymptom(Input, Question) :-
+    write(Question),
+    read(X),
+    (
+        (
+            X = 'y' -> 
+            (
+                assert(tb(1)),
+                count_predicates(Count),
+                format('TB ~w', [Count])
+            )
+        );
+        X = 'n';
+        (
+            X = Y ->
+            (
+                format('Invalid input. Please enter \'y\' or \'n\'.'), nl,
+                askSymptom(Input, Question)
+            )
+        )
+    ).
+
+:- dynamic(tb/1).
+
+:- initialization(main).
+main :- 
+    /*
+        If you have tuberculosis, add 1 to TB score.
+    */
+    demographics(Name, Age, Sex),
+    askSymptom(Input, ('Do you have a cough? ')).
