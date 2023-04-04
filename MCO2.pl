546bdb92215d9cd82f0d58dfb403751f4923c038
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-04-04 12:10:15 +0800

    3rd Commit

--- MCO2.pl
+++ MCO2.pl
@@ -1,7 +1,34 @@
+/**
+*   MCO2 - ChatBot
+*   Section: S15
+*   Author: 
+*       BACOSA, Gabriel
+*       CUALES, Bianca 
+*       LASALA, Kyle
+*       REYNADO, Alyza
+*   
+*   Description:
+*       This is a chatbot that will ask the user about their symptoms and will
+*       provide a possible diagnosis based on the answers given.
+*/
+
+/**
+*   Parameters:
+*       Count - The number of predicates that are currently asserted
+*   Description:
+*       Counts the number of predicates that are currently asserted
+*/
 count_predicates(Count) :-
     findall(_, tb(_), Predicates),
     length(Predicates, Count).
 
+/**
+*   Parameters:
+*       Name - The name of the patient
+*       Age - The age of the patient
+*   Description:
+*       Asks the patient for their name, age, and sex
+*/
 demographics(Name, Age) :-
     write('What is your name? '),
     read(Name),
@@ -9,19 +36,54 @@ demographics(Name, Age) :-
     read(Age),
     askSex.
 
+/**
+*   Description:
+*       Helper function for knowing and checking the input for sex
+*/
 askSex :-
     write('What is your sex? (m/f) '),
     read(Input),
     (
         Input = 'm' -> assert(sex(male));
         Input = 'f' -> assert(sex(female));
-        Input = X -> 
         (
             format('Invalid input. Please enter \'m\' or \'f\'.'), nl,
             askSex
         )
     ).
 
+/**
+*   Parameters:
+*       Name - The name of the patient
+*   Description:
+*       Introduces the chatbot and provides a brief description of its purpose
+*/
+introduction :-   
+    nl,
+    write('========================================================================'), nl,
+    write('                                 MEDIBOT'), nl,
+    write('========================================================================'), nl,
+    write('Hello! I am MediBot! Your personal health care companion!'), nl,nl,
+
+    write('I\'m here to help you find out what might be causing your symptoms'), nl,
+    write('and provide you with some guidance on what steps to take next.'), nl,
+    write('Simply tell me about your symptoms and answer any questions I may'), nl,
+    write('have, and I\'ll use my knowledge to provide you with a possible '), nl,
+    write('diagnosis. Please keep in mind that while I can provide some insights,'), nl,
+    write('I am not a substitute for professional medical advice, and it\'s '), nl,
+    write('important to always consult with a qualified healthcare provider '), nl,
+    write('for proper diagnosis and treatment. Let\'s get started!'), nl,
+    write('========================================================================'), nl, nl,
+
+    write('If you have read and understood, please type anything to continue'), nl,
+    write('followed by a period (.) and press enter.'), nl,
+    read(_), nl, nl,
+
+    write('========================================================================'), nl,
+    write('                              INSTRUCTIONS'), nl,
+    write('========================================================================'), nl,
+    write('').
+    
 
 askSymptom(Question, Predicate) :-
     write(Question),
@@ -37,11 +99,8 @@ askSymptom(Question, Predicate) :-
         );
         Input = 'n';
         (
-            Input = Y ->
-            (
-                format('Invalid input. Please enter \'y\' or \'n\'.'), nl,
-                askSymptom(Question, Predicate)
-            )
+            format('Invalid input. Please enter \'y\' or \'n\'.'), nl,
+            askSymptom(Question, Predicate)
         )
     ).
 
@@ -51,5 +110,12 @@ askSymptom(Question, Predicate) :-
 :- initialization(main).
 main :- 
     % Ask for the patient's name, age, and sex
+    introduction.
+    % demographics(Name, Age).
     demographics(Name, Age),
-    askSymptom(('Do you have a cough? '), tb(1)).
+    write('========================================================================'), nl,
+    write('                          PATIENT INFORMATION'), nl,
+    write('========================================================================'), nl,
+    format('Name: ~w', [Name]), nl,
+    format('Age: ~w', [Age]), nl,
+    format().
